dd355b8745bf0f1a827ad47d635378a2a186dac4
nguyenbinh2003 <nguyenbinh191103@gmail.com>
2023-07-27 21:16:23 +0700

    merge

--- src/App.js
+++ src/App.js
@@ -1,28 +1,79 @@
-import './App.css';
-import { Routes, Route } from "react-router-dom";
-import Rooms from './components/Rooms/Rooms';
-import Home from './pages/HomePage/Home';
-import Header from './components/Layout/Header';
-import Footer from './components/Layout/Footer';
-import LoginPage from './pages/LoginPage/LoginPage';
-import ContactsPage from './pages/ContactsPage/ContactsPage';
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Rooms from "./components/Rooms/Rooms";
+import Home from "./pages/HomePage/Home";
+import Header from "./components/Layout/Header";
+import Footer from "./components/Layout/Footer";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ContactsPage from "./pages/ContactsPage/ContactsPage";
+import MyApp from "./contexts/MyApp";
 
 function App() {
+  const [usersData, setUsersData] = useState([]);
+  const [dataUser, setDataUser] = useState([]);
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const inputRef = useRef(null);
+  const navigate = useNavigate();
+
+  const onUserEmailValueChange = (e) => {
+    setUserEmail(e.target.value);
+  };
+  const onUserPasswordValueChange = (e) => {
+    setUserPassword(e.target.value);
+  };
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await fetch(
+        "https://64b3e84f0efb99d862688494.mockapi.io/userData"
+      );
+      const jsonData = await response.json();
+      setUsersData(jsonData);
+    };
+    getUser();
+  }, []);
+
+  const onSubmitLoginHandle = (e) => {
+    e.preventDefault();
+    const findUser = usersData.find((user) => {
+      return userEmail === user.email_address && userPassword === user.password;
+    });
+    console.log(findUser);
+    if (findUser) {
+      localStorage.setItem("dataUser", JSON.stringify(findUser));
+      const storedDataUser = JSON.parse(localStorage.getItem("dataUser"));
+      setDataUser(storedDataUser);
+      navigate("/");
+    } else {
+      alert("Your email address and password are incorrect, please re-enter !");
+    }
+  };
+
   return (
-    <div className="App">
-
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/rooms" element={<Rooms />} />
-        <Route path="/cart" element={<Home />} />
-        <Route path="/contact" element={<ContactsPage />} />
-      </Routes>
-      <Footer />
-    </div>
-
-    
+    <MyApp.Provider
+      value={{
+        usersData: usersData,
+        dataUser: dataUser,
+        userEmail: userEmail,
+        userPassword: userPassword,
+        onUserEmailValueChange,
+        onUserPasswordValueChange,
+        onSubmitLoginHandle,
+      }}
+    >
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/rooms" element={<Rooms />} />
+          <Route path="/cart" element={<Home />} />
+          <Route path="/contact" element={<ContactsPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </MyApp.Provider>
   );
 }
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,65 +1,128 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <header>
-            <nav className="navbar navbar-dark bg-dark fixed-top">
-                <div className="container">
-                    <a className="navbar-brand" href="./">
-                        <i className="fas fa-home"> Home <span style={{ fontStyle: 'italic', fontWeight: 100 }}>Stays</span>
-                        </i>
-                    </a>
-                    <div className="list-inline-border">
-                        <li>
-                            <i className="fas fa-pencil-alt" /> BOOK NOW
-                        </li>
-                        <li>
-                            <i className="fas fa-phone" /> +84 123 456 789
-                        </li>
-                    </div>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon" />
-                    </button>
-                    <div className="offcanvas offcanvas-end text-bg-dark" tabIndex={-1} id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                        <div className="offcanvas-header">
-                            <a className="offcanvas-title navbar-brand" id="offcanvasDarkNavbarLabel" href="./index.html">
-                                <i className="fas fa-home"> Home <span style={{ fontStyle: 'italic', fontWeight: 100 }}>Stays</span>
-                                </i>
-                            </a>
-                            <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close" />
-                        </div>
-                        <div className="offcanvas-body">
-                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">HOME</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="./rooms.html">ROOMS</a>
-                                </li>
-                                <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        PAGES
-                                    </a>
-                                    <ul className="dropdown-menu dropdown-menu-dark">
-                                        <li><a className="dropdown-item" href="#">Blog</a></li>
-                                        <li><a className="dropdown-item" href="#">Team</a></li>
-                                    </ul>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">CONTACT</a>
-                                </li>
-                            </ul>
-                            <form className="d-flex mt-3" role="search">
-                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                <button className="btn btn-web" type="submit">Search</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </header>
+  return (
+    <header>
+      <nav className="navbar navbar-dark bg-dark fixed-top">
+        <div className="container">
+          <a className="navbar-brand" href="./">
+            <i className="fas fa-home">
+              {" "}
+              Home{" "}
+              <span style={{ fontStyle: "italic", fontWeight: 100 }}>
+                Stays
+              </span>
+            </i>
+          </a>
+          <div className="list-inline-border">
+            <li>
+              <i className="fas fa-pencil-alt" /> BOOK NOW
+            </li>
+            <li>
+              <i className="fas fa-phone" /> +84 123 456 789
+            </li>
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasDarkNavbar"
+            aria-controls="offcanvasDarkNavbar"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div
+            className="offcanvas offcanvas-end text-bg-dark"
+            tabIndex={-1}
+            id="offcanvasDarkNavbar"
+            aria-labelledby="offcanvasDarkNavbarLabel"
+          >
+            <div className="offcanvas-header">
+              <a
+                className="offcanvas-title navbar-brand"
+                id="offcanvasDarkNavbarLabel"
+                href="./index.html"
+              >
+                <i className="fas fa-home">
+                  {" "}
+                  Home{" "}
+                  <span style={{ fontStyle: "italic", fontWeight: 100 }}>
+                    Stays
+                  </span>
+                </i>
+              </a>
+              <button
+                type="button"
+                className="btn-close btn-close-white"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              />
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    HOME
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="./rooms.html">
+                    ROOMS
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    PAGES
+                  </a>
+                  <ul className="dropdown-menu dropdown-menu-dark">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Blog
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Team
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Link to='/contact' className="nav-link">
+                    CONTACT
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to='/login' className="nav-link">
+                    LOGIN
+                  </Link>
+                </li>
+              </ul>
+              <form className="d-flex mt-3" role="search">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button className="btn btn-web" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-    )
-}
-
-export default Header
+export default Header;
