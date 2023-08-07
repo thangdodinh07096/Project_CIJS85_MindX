0d78017e49d94d2f899ef7ca061f30781e66a5ae
thangdodinh07096 <thangdodinh07096>
2023-08-08 00:19:53 +0700

    fix conflic

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,115 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
-
-const Layout = () => {
-    const activeClass = (params) => {
-        return params.isActive ? 'active-item' : '';
-    };
-    return (
-        <div>
-            <header>
-                <nav className="navbar navbar-dark bg-dark fixed-top">
-                    <div className="container">
-                        <a className="navbar-brand" href="./">
-                            <i className="fas fa-home"> Home <span style={{ fontStyle: 'italic', fontWeight: 100 }}>Stays</span>
-                            </i>
-                        </a>
-                        <div className="list-inline-border">
-                            <li>
-                                <i className="fas fa-pencil-alt" /> BOOK NOW
-                            </li>
-                            <li>
-                                <i className="fas fa-phone" /> +84 123 456 789
-                            </li>
-                        </div>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon" />
-                        </button>
-                        <div className="offcanvas offcanvas-end text-bg-dark" tabIndex={-1} id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                            <div className="offcanvas-header">
-                                <a className="offcanvas-title navbar-brand" id="offcanvasDarkNavbarLabel" href="./index.html">
-                                    <i className="fas fa-home"> Home <span style={{ fontStyle: 'italic', fontWeight: 100 }}>Stays</span>
-                                    </i>
-                                </a>
-                                <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close" />
-                            </div>
-                            <div className="offcanvas-body">
-                                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                    <li className="nav-item">
-                                        <NavLink to="/" className={activeClass}>HOME</NavLink>
-                                        {/* <a className="nav-link active" aria-current="page" href="#">HOME</a> */}
-                                    </li>
-                                    <li className="nav-item">
-                                        <NavLink to="/hotels" className={activeClass}>BOOKING</NavLink>
-                                        {/* <a className="nav-link" href="./rooms.html">ROOMS</a> */}
-                                    </li>
-                                    <li className="nav-item">
-                                        <NavLink to="/about-us" className={activeClass}>ABOUT US</NavLink>
-                                        {/* <a className="nav-link" href="./rooms.html">ROOMS</a> */}
-                                    </li><li className="nav-item">
-                                        <NavLink to="/blogs" className={activeClass}>BLOG</NavLink>
-                                        {/* <a className="nav-link" href="./rooms.html">ROOMS</a> */}
-                                    </li><li className="nav-item">
-                                        <NavLink to="/contact" className={activeClass}>CONTACT</NavLink>
-                                        {/* <a className="nav-link" href="./rooms.html">ROOMS</a> */}
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </header>
-            <Outlet />
-            <footer id="footer" className=" bg-dark">
-                <div className="container ">
-                    <div className="row justify-content-center">
-                        <div className="col-md-12 text-center">
-                            <h2 className="footer-heading">
-                                <a className="navbar-brand" href="./index.html">
-                                    <i className="fas fa-home"> Home <span style={{ fontStyle: 'italic', fontWeight: 100 }}>Stays</span>
-                                    </i>
-                                </a>
-                            </h2>
-                            <p className="menu">
-                                <a href="#">Home</a>
-                                <a href="#">Team</a>
-                                <a href="#">About</a>
-                                <a href="#">Rooms</a>
-                                <a href="#">Blog</a>
-                                <a href="#">Contact</a>
-                            </p>
-                            <ul className="ftco-footer-social p-0">
-                                <li className="ftco-animate">
-                                    <a href="#" data-toggle="tooltip" data-placement="top" title="Twitter">
-                                        <i className="fab fa-twitter" />
-                                    </a>
-                                </li>
-                                <li className="ftco-animate">
-                                    <a href="#" data-toggle="tooltip" data-placement="top" title="Facebook">
-                                        <i className="fab fa-facebook" />
-                                    </a>
-                                </li>
-                                <li className="ftco-animate">
-                                    <a href="#" data-toggle="tooltip" data-placement="top" title="Instagram">
-                                        <i className="fab fa-instagram" />
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="row mt-5">
-                        <div className="col-md-12 text-center">
-                            <p className="copyright">
-                                Copyright ©
-                                All rights reserved | This template
-                                is made with <i className="ion-ios-heart" aria-hidden="true" /> by <a href="./index.html" target="_blank">HomeStays.com</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-=======
 import React, { useContext } from "react";
 import { Link, NavLink, Outlet } from "react-router-dom";
 import MyApp from "../../contexts/MyApp";
@@ -169,11 +57,9 @@ const Layout = () => {
               aria-labelledby="offcanvasDarkNavbarLabel"
             >
               <div className="offcanvas-header">
-                <a
+                <Link to="/"
                   className="offcanvas-title navbar-brand"
-                  id="offcanvasDarkNavbarLabel"
-                  href="./index.html"
-                >
+                  id="offcanvasDarkNavbarLabel">
                   <i className="fas fa-home">
                     {" "}
                     Home{" "}
@@ -181,7 +67,7 @@ const Layout = () => {
                       Stays
                     </span>
                   </i>
-                </a>
+                </Link>
                 <button
                   type="button"
                   className="btn-close btn-close-white"
@@ -197,9 +83,9 @@ const Layout = () => {
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="./rooms.html">
-                      ROOMS
-                    </a>
+                    <Link to="/hotels" className="nav-link">
+                      BOOKING HOTEL
+                    </Link>
                   </li>
                   <li className="nav-item dropdown">
                     <a
@@ -330,9 +216,10 @@ const Layout = () => {
               </p>
             </div>
           </div>
->>>>>>> main
         </div>
-    );
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
